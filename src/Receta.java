221e9cef5263292c1aceb8f13d478948052f52ad
Gabriel-2A <gaa.021205@gmail.com>
2022-03-10 11:48:50 -0600

    Creasion de la clase Receta

--- src/Receta.java
+++ src/Receta.java
@@ -1,7 +1,39 @@
+import java.util.ArrayList;
+
 public class Receta {
 
-    private Ingrediente[] ing = new Ingrediente[10];
-    private String[] pasos;
+    private ArrayList<Ingrediente> Ingredientes;
+    private ArrayList<String> pasos;
     private double tiempoPreparacion;
 
+    public Receta(ArrayList<Ingrediente> ingredientes, ArrayList<String> pasos, double tiempoPreparacion) {
+        Ingredientes = ingredientes;
+        this.pasos = pasos;
+        this.tiempoPreparacion = tiempoPreparacion;
+        System.out.println("Receta creada");
+    }
+
+    public ArrayList<Ingrediente> getIngredientes() {
+        return Ingredientes;
+    }
+
+    public void setIngredientes(ArrayList<Ingrediente> ingredientes) {
+        Ingredientes = ingredientes;
+    }
+
+    public ArrayList<String> getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(ArrayList<String> pasos) {
+        this.pasos = pasos;
+    }
+
+    public double getTiempoPreparacion() {
+        return tiempoPreparacion;
+    }
+
+    public void setTiempoPreparacion(double tiempoPreparacion) {
+        this.tiempoPreparacion = tiempoPreparacion;
+    }
 }
