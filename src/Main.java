56dffba3910e09835b8c565346907371234bbca9
Gabriel-2A <gaa.021205@gmail.com>
2022-03-15 11:53:27 -0600

    creacion de ingredientes y receta en la clase main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public  static void main(String[] args){
-        //Ingrediente Ingre = new Ingrediente();
+        //Ingredientes
+        Ingrediente sal = new Ingrediente("Sal", true, 200);
+        Ingrediente jitomate = new Ingrediente("Jitomate", false, 250);
+        Ingrediente pasta = new Ingrediente("Pasta", false, 1000);
+        Ingrediente agua = new Ingrediente("Agua", false, 1000);
+
+        ArrayList<Ingrediente> ingredientesDisponibles = new ArrayList<>(Arrays.asList(
+                sal, jitomate, agua ));
+
+        Receta salsa = new Receta(
+                "Salsa",
+                new ArrayList<>(Arrays.asList(sal, jitomate, agua)),
+                new ArrayList<>(Arrays.asList("Lavar los Ingredientes",
+                        "cocer el jitomate",
+                        "Moler el jitomate con chile y ajo y cebolla")
+                        ),
+                15.50f
+        );
     }
 }
